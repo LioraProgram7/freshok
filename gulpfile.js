c5e721de99ca5be6a139c10cc6d083475b3adcd3
LioraProgram7 <sv.kiselis@gmail.com>
2022-02-02 12:46:36 +0200

    added footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
-const { src, dest, watch, parallel, series } = require('gulp');
-const scss         = require('gulp-sass')(require('sass'));
-const concat       = require('gulp-concat');
+const {
+  src,
+  dest,
+  watch,
+  parallel,
+  series
+} = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
-const uglify       = require('gulp-uglify');
-const imagemin     = require('gulp-imagemin');
-const del          = require('del');
-var browserSync    = require('browser-sync').create();
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+var browserSync = require('browser-sync').create();
 
 function browsersync() {
   browserSync.init({
@@ -18,7 +24,9 @@ function browsersync() {
 
 function styles() {
   return src('app/scss/style.scss')
-    .pipe(scss({ outputStyle: 'expanded'}).on('error', scss.logError))
+    .pipe(scss({
+      outputStyle: 'expanded'
+    }).on('error', scss.logError))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['Last 10 versions'],
@@ -30,38 +38,50 @@ function styles() {
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
-    'app/js/main.js'
-  ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream());
+      'node_modules/jquery/dist/jquery.js',
+      'app/js/main.js'
+    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream());
 }
 
 function images() {
   return src('app/images/**/*.*')
-  .pipe(imagemin([
-    imagemin.gifsicle({ interlaced: true }),
-    imagemin.mozjpeg({ quality: 75, progressive: true }),
-    imagemin.optipng({ optimizationLevel: 5 }),
-    imagemin.svgo({ 
-      plugins: [
-        { removeViewBox: true },
-        { cleanupIDs: false }
-      ]
-    })
-  ]))
-  .pipe(dest('dist/images'));
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.mozjpeg({
+        quality: 75,
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
+      imagemin.svgo({
+        plugins: [{
+            removeViewBox: true
+          },
+          {
+            cleanupIDs: false
+          }
+        ]
+      })
+    ]))
+    .pipe(dest('dist/images'));
 }
 
 function build() {
-  return src ([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], {base: 'app'})
-  .pipe(dest('dist'));
+  return src([
+      'app/**/*.html',
+      'app/css/style.min.css',
+      'app/js/main.min.js'
+    ], {
+      base: 'app'
+    })
+    .pipe(dest('dist'));
 }
 
 function cleanDist() {
@@ -74,12 +94,12 @@ function watching() {
   watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
-exports.styles      = styles;
-exports.scripts     = scripts;
+exports.styles = styles;
+exports.scripts = scripts;
 exports.browsersync = browsersync;
-exports.watching    = watching;
-exports.images      = images;
-exports.cleanDist   = cleanDist;
-exports.build       = series(cleanDist, images, build);
+exports.watching = watching;
+exports.images = images;
+exports.cleanDist = cleanDist;
+exports.build = series(cleanDist, images, build);
 
 exports.default = parallel(styles, scripts, browsersync, watching);
