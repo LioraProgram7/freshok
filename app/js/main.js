3d5135fef933b3f249d475281c249207b89b6d38
LioraProgram7 <sv.kiselis@gmail.com>
2022-02-04 23:50:18 +0200

    added service section

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,12 @@
 $(function () {
 
-  // $('.slider-top__inner').slick({
-    
-  // });
+  $('.slider-top__inner').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+
+    autoplay: true,
+    autoplaySpead: 2000
+
+  });
 
 });
