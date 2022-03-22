8d1e9816ce7411c9756507de96dc3df5ef1eef52
LioraProgram7 <sv.kiselis@gmail.com>
2022-03-22 16:24:09 +0200

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,86 @@
 $(function () {
 
+  $('.related-product__items').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev-three"><svg width="20" height="32" viewBox="0 0 20 32" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d = "M18.9641 14.9851L2.95082 0.392105C2.42535 -0.130702 1.57314 -0.130702 1.04767 0.392105C0.522196 0.914872 0.522196 1.76312 1.04767 2.28593L16.0954 16L1.049 29.7141C0.523529 30.2369 0.523529 31.0852 1.049 31.6079C1.57448 32.1307 2.42668 32.1307 2.95212 31.6079L18.9654 17.0149C19.2455 16.7362 19.3656 16.3668 19.3469 16.0014C19.3643 15.6346 19.2443 15.2652 18.9641 14.9851Z"fill = "#505050" / ></svg></button>',
+
+    nextArrow: '<button type="button" class="slick-next-three"><svg width="20" height="32" viewBox="0 0 20 32" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d = "M1.03822 17.0149L17.0515 31.6079C17.577 32.1307 18.4292 32.1307 18.9547 31.6079C19.4801 31.0851 19.4801 30.2369 18.9547 29.7141L3.90692 16L18.9533 2.2859C19.4788 1.76309 19.4788 0.914839 18.9533 0.392073C18.4279 -0.130695 17.5757 -0.130695 17.0502 0.392072L1.03689 14.9851C0.75682 15.2638 0.636745 15.6332 0.655452 15.9986C0.638038 16.3654 0.758075 16.7348 1.03822 17.0149Z"fill = "#505050" / ></svg></button>',
+    responsive: [{
+        breakpoint: 1290,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 2
+        }
+      },
+    ]
+  });
+
+  $('.product-slide__list').slick({
+    infinite: false,
+    prevArrow: '<button type="button" class="slick-prev-two"><svg width="20" height="32" viewBox="0 0 20 32" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d = "M18.9641 14.9851L2.95082 0.392105C2.42535 -0.130702 1.57314 -0.130702 1.04767 0.392105C0.522196 0.914872 0.522196 1.76312 1.04767 2.28593L16.0954 16L1.049 29.7141C0.523529 30.2369 0.523529 31.0852 1.049 31.6079C1.57448 32.1307 2.42668 32.1307 2.95212 31.6079L18.9654 17.0149C19.2455 16.7362 19.3656 16.3668 19.3469 16.0014C19.3643 15.6346 19.2443 15.2652 18.9641 14.9851Z"fill = "#505050" / ></svg></button>',
+
+    nextArrow: '<button type="button" class="slick-next-two"><svg width="20" height="32" viewBox="0 0 20 32" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d = "M1.03822 17.0149L17.0515 31.6079C17.577 32.1307 18.4292 32.1307 18.9547 31.6079C19.4801 31.0851 19.4801 30.2369 18.9547 29.7141L3.90692 16L18.9533 2.2859C19.4788 1.76309 19.4788 0.914839 18.9533 0.392073C18.4279 -0.130695 17.5757 -0.130695 17.0502 0.392072L1.03689 14.9851C0.75682 15.2638 0.636745 15.6332 0.655452 15.9986C0.638038 16.3654 0.758075 16.7348 1.03822 17.0149Z"fill = "#505050" / ></svg></button>',
+
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+      }
+    }]
+  });
+
+  $('.product-slide__list').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+  });
+
+
+
   // табы странички товара
   $('.product-tabs__top-item').on('click', function (e) {
     e.preventDefault();
     $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
     $(this).addClass('product-tabs__top-item--active');
-    
+
     $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
     $($(this).attr('href')).addClass('product-tabs__content-item--active');
   });
 
 
-
-// звездный рейтинг
-   $(".star").rateYo({
+  // звездный рейтинг
+  $(".star").rateYo({
     //  rating: 4,
-     starWidth: "16px",
-      normalFill: "rgba(193, 193, 193, 0.3)",
-      ratedFill: "#FFB800",
-      spacing: "6px",
+    starWidth: "16px",
+    normalFill: "rgba(193, 193, 193, 0.3)",
+    ratedFill: "#FFB800",
+    spacing: "6px",
 
-      // КОГДА ВСТАВЛЯЮ КАРТИНКУ НЕ РАБОТАЕТ РЕЙТИНГ???????????????????????????
-      // starSvg: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d = "M0.0229731 6.16426C0.0780973 5.99454 0.224753 5.87085 0.401315 5.84523L5.36139 5.12445L7.57966 0.629872C7.6586 0.469872 7.82157 0.368591 7.99997 0.368591C8.17841 0.368591 8.34135 0.469872 8.42032 0.629872L10.6387 5.12445L15.5987 5.84523C15.7752 5.87085 15.9219 5.99454 15.977 6.16422C16.0322 6.33394 15.9862 6.52022 15.8584 6.64475L12.2694 10.1433L13.1165 15.0834C13.1467 15.2593 13.0744 15.4369 12.9301 15.5418C12.8484 15.6012 12.7517 15.6313 12.6545 15.6313C12.5799 15.6313 12.505 15.6136 12.4365 15.5775L8 13.2451L3.56374 15.5775C3.40577 15.6605 3.21443 15.6467 3.07009 15.5418C2.92574 15.4369 2.8534 15.2592 2.88356 15.0833L3.73096 10.1433L0.141566 6.64472C0.0138168 6.52022 -0.0322151 6.33394 0.0229731 6.16426Z"fill = "#FFB800" / ></svg>'
-   });
+    // КОГДА ВСТАВЛЯЮ КАРТИНКУ НЕ РАБОТАЕТ РЕЙТИНГ/ изначально работал. Причину сбоя не нашла. В документации плагина Rate Yo! смотрела по инструкции сделала правильно
+    starSvg: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d = "M0.0229731 6.16426C0.0780973 5.99454 0.224753 5.87085 0.401315 5.84523L5.36139 5.12445L7.57966 0.629872C7.6586 0.469872 7.82157 0.368591 7.99997 0.368591C8.17841 0.368591 8.34135 0.469872 8.42032 0.629872L10.6387 5.12445L15.5987 5.84523C15.7752 5.87085 15.9219 5.99454 15.977 6.16422C16.0322 6.33394 15.9862 6.52022 15.8584 6.64475L12.2694 10.1433L13.1165 15.0834C13.1467 15.2593 13.0744 15.4369 12.9301 15.5418C12.8484 15.6012 12.7517 15.6313 12.6545 15.6313C12.5799 15.6313 12.505 15.6136 12.4365 15.5775L8 13.2451L3.56374 15.5775C3.40577 15.6605 3.21443 15.6467 3.07009 15.5418C2.92574 15.4369 2.8534 15.2592 2.88356 15.0833L3.73096 10.1433L0.141566 6.64472C0.0138168 6.52022 -0.0322151 6.33394 0.0229731 6.16426Z"fill = "#FFB800" / ></svg>'
+  });
 
 
   // $('.product-slide__list').slick({
@@ -37,7 +95,7 @@ $(function () {
   $('.filter-item__btn').on('click', function () {
     $('.filter-categories__list').slideToggle();
     // пыталась сделать чтобы стрелка при открытии поворачивалась но не работает
-       $(this).toggleClass('transform');
+    $(this).toggleClass('transform');
   });
 
   $('.filter-item__btn--click1').on('click', function () {
@@ -51,7 +109,7 @@ $(function () {
   $('.filter-item__btn--click3').on('click', function () {
     $('.filter-price__form').slideToggle();
   });
-  
+
 
 
   // кнопки переключения вида страниц каталога товаров
@@ -107,10 +165,10 @@ $(function () {
     $('body').toggleClass('overlay');
   });
 
-   $('.catalog-page__btn').on('click', function () {
-     $('.catalog-page__filter').removeClass('active');
-     $('body').toggleClass('overlay');
-   });
+  $('.catalog-page__btn').on('click', function () {
+    $('.catalog-page__filter').removeClass('active');
+    $('body').toggleClass('overlay');
+  });
 
 
   $('.header-bottom__user-item--search').on('click', function () {
--- gulpfile.js
+++ gulpfile.js
@@ -47,6 +47,7 @@ function scripts() {
       'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
       'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
       'node_modules/rateyo/src/jquery.rateyo.js',
+      'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
