d023f4afa277d77368f6e3d0dc02cc75c54d29ea
LioraProgram7 <sv.kiselis@gmail.com>
2022-03-23 13:05:06 +0200

    correrted catalog-page, product-page

--- app/js/main.js
+++ app/js/main.js
@@ -45,17 +45,17 @@ $(function () {
     }]
   });
 
-  $('.product-slide__list').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-  });
+  // $('.product-slide__list').magnificPopup({
+  //   delegate: 'a',
+  //   type: 'image',
+  //   tLoading: 'Loading image #%curr%...',
+  //   mainClass: 'mfp-img-mobile',
+  //   gallery: {
+  //     enabled: true,
+  //     navigateByImgClick: true,
+  //     preload: [0, 1]
+  //   },
+  // });
 
 
 
@@ -81,14 +81,6 @@ $(function () {
     // КОГДА ВСТАВЛЯЮ КАРТИНКУ НЕ РАБОТАЕТ РЕЙТИНГ/ изначально работал. Причину сбоя не нашла. В документации плагина Rate Yo! смотрела по инструкции сделала правильно
     starSvg: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d = "M0.0229731 6.16426C0.0780973 5.99454 0.224753 5.87085 0.401315 5.84523L5.36139 5.12445L7.57966 0.629872C7.6586 0.469872 7.82157 0.368591 7.99997 0.368591C8.17841 0.368591 8.34135 0.469872 8.42032 0.629872L10.6387 5.12445L15.5987 5.84523C15.7752 5.87085 15.9219 5.99454 15.977 6.16422C16.0322 6.33394 15.9862 6.52022 15.8584 6.64475L12.2694 10.1433L13.1165 15.0834C13.1467 15.2593 13.0744 15.4369 12.9301 15.5418C12.8484 15.6012 12.7517 15.6313 12.6545 15.6313C12.5799 15.6313 12.505 15.6136 12.4365 15.5775L8 13.2451L3.56374 15.5775C3.40577 15.6605 3.21443 15.6467 3.07009 15.5418C2.92574 15.4369 2.8534 15.2592 2.88356 15.0833L3.73096 10.1433L0.141566 6.64472C0.0138168 6.52022 -0.0322151 6.33394 0.0229731 6.16426Z"fill = "#FFB800" / ></svg>'
   });
-
-
-  // $('.product-slide__list').slick({
-  //       infinite: true,
-  //       speed: 300,
-  //       slidesToShow: 1,
-  //       adaptiveHeight: true
-  // });
 
 
   // для категорий чтобы скрыть 
@@ -132,18 +124,64 @@ $(function () {
 
 
 
-  // для цены ползунок
-  $('.filter-price__input').ionRangeSlider({
-    type: "double",
-    onStart: function (data) {
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
-    },
+  // фильтер цены
+  var $range = $('.filter-price__input'),
+    $inputFrom = $('.filter-price__from'),
+    $inputTo = $('.filter-price__to'),
+    instance,
+    min = 0,
+    max = 1150,
+    from = 0,
+    to = 0;
 
-    onChange: function (data) {
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
-    },
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    from: 100,
+    to: 1000,
+    onStart: updateInputs,
+    onChange: updateInputs
+  });
+  instance = $range.data("ionRangeSlider");
+
+  function updateInputs(data) {
+    from = data.from;
+    to = data.to;
+
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);
+  }
+
+  $inputFrom.on("input", function () {
+    var val = $(this).prop("value");
+
+    // validate
+    if (val < min) {
+      val = min;
+    } else if (val > to) {
+      val = to;
+    }
+
+    instance.update({
+      from: val
+    });
+  });
+
+  $inputTo.on("input", function () {
+    var val = $(this).prop("value");
+
+    // validate
+    if (val < from) {
+      val = from;
+    } else if (val > max) {
+      val = max;
+    }
+
+    instance.update({
+      to: val
+    });
   });
 
 
